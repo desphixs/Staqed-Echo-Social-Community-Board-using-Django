26ca9eed214ba37faff69f4b3868c765b9ebcfe1
destiny <desphixs@gmail.com>
2026-05-01 01:00:00 +0100

    passed data from view to template via context dictionary

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'posts/index.html')
+    # Define a list of hardcoded topics (our "menu" of data)
+    topics = [
+        {
+            'title': 'How to learn Django fast?',
+            'content': "I'm just starting out and wanted to know the best resources to master Django in a few weeks."
+        },
+        {
+            'title': 'The future of web development',
+            'content': "With AI on the rise, what do you think the next 5 years look like for junior developers?"
+        },
+        {
+            'title': 'My first Echo post!',
+            'content': "Just testing out this cool new platform I'm building. It feels great to see it coming together!"
+        },
+    ]
+    
+    # Put the data into a "context" dictionary so the template can see it
+    context = {
+        'topics': topics
+    }
+    
+    # Send the request, the template path, and the context data to the browser
+    return render(request, 'posts/index.html', context)
