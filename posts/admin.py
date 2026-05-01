f1a3fe95cf1840b290392209ac0fb453b2161a0b
destiny <desphixs@gmail.com>
2026-05-01 01:12:21 +0100

    created django models and admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# This makes the Post model visible and editable in the Admin panel
+admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # A short title for the community post
+    title = models.CharField(max_length=200)
+    
+    # The actual content of the post
+    content = models.TextField()
+    
+    # Automatically set the date and time when the post is first created
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # This tells Django what to show in the Admin panel list
+        return self.title
